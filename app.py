142c60827b477a181c63a1ffef6574a6a590d10c
yuvalbenar <yuvalbenar@gmail.com>
2025-01-15 13:58:49 +0200

    Update app.py port and yaml port

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def index():
     return render_template('index.html', weather=weather, facts=winter_facts, city_country=city_country)
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port=5001)
+    app.run(debug=True,host="0.0.0.0",port=5000)
